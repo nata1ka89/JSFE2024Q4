4888866a5403194d897d44dab2157feffeae4cc5
nata1ka <nata1ka.yurevich@gmail.com>
2025-01-08 22:15:19 +0300

    feat: create element round, create buttonslevel, buttonsgame and their style

--- simon-says/script.js
+++ simon-says/script.js
@@ -1,13 +1,17 @@
 const bodyElement = document.body;
 
 function createElement(options) {
-  const { tag = '', text = '', parent, classes = [] } = options;
+  const { tag = '', text = '', parent, classes = [], id = '' } = options;
 
   const element = document.createElement(tag);
   element.textContent = text;
 
   if (classes.length > 0) {
     element.classList.add(...classes);
+  }
+
+  if (id !== '') {
+    element.id = id;
   }
 
   if (parent != null) {
@@ -17,10 +21,61 @@ function createElement(options) {
   return element;
 }
 
+// Header
+const header = createElement({
+  tag: 'header',
+  parent: bodyElement
+});
+
+function createHeader() {
+  //create level button
+  const divLevel = createElement({
+    tag: 'div',
+    classes: ['container-level'],
+    parent: header
+  });
+
+  const levels = ['Easy', 'Medium', 'Hard'];
+
+  levels.forEach(level => {
+    const buttonLevel = createElement({
+      tag: 'button',
+      text: level,
+      parent: divLevel,
+      classes: ['button-level'],
+      id: level
+    });
+  });
+
+  //create round
+  const divRound = createElement({
+    tag: 'div',
+    classes: ['container-round'],
+    parent: header
+  });
+
+  const textSpan = createElement({
+    tag: 'span',
+    text: 'Round',
+    parent: divRound
+  });
+  const count = createElement({
+    tag: 'span',
+    text: '1',
+    parent: divRound
+  });
+}
+// Main
+const main = createElement({
+  tag: 'main',
+  parent: bodyElement
+});
+
+// create Keyboard num
 function createKeyboardNum() {
   const container = createElement({
     tag: 'div',
-    parent: bodyElement,
+    parent: main,
     classes: ['keyboard-num']
   });
 
@@ -33,13 +88,17 @@ function createKeyboardNum() {
       parent: container,
       classes: ['button']
     });
+    button.addEventListener('click', () => {
+      button.classList.toggle('button-active');
+    });
   });
 }
 
+// create Keyboard letter
 function createKeyboardLetter() {
   const container = createElement({
     tag: 'div',
-    parent: bodyElement,
+    parent: main,
     classes: ['keyboard-letter']
   });
 
@@ -52,8 +111,37 @@ function createKeyboardLetter() {
       parent: container,
       classes: ['button']
     });
+    button.addEventListener('click', () => {
+      button.classList.toggle('button-active');
+    });
   });
 }
 
+// create "Start" button,"Repeat the sequence" button, "New game" button
+
+function createGameButton() {
+  //create Game button
+  const divGame = createElement({
+    tag: 'div',
+    classes: ['container-game'],
+    parent: main
+  });
+
+  const buttonsGame = ['Repeat the sequence', 'New game', 'Start'];
+
+  buttonsGame.forEach(button => {
+    const buttonId = button.replace(/\s+/g, '-');
+    const buttonGame = createElement({
+      tag: 'button',
+      text: button,
+      parent: divGame,
+      classes: ['button-game'],
+      id: buttonId
+    });
+  });
+}
+
+createHeader();
 createKeyboardNum();
 createKeyboardLetter();
+createGameButton();
