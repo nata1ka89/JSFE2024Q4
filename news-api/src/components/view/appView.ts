4f4524af13fd7ec04f1b4480fa1c226c0235009d
nata1ka <nata1ka.yurevich@gmail.com>
2025-02-20 15:59:07 +0300

    refactor: rename file appView.js, type code

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,18 +1,21 @@
 import News from './news/news';
 import Sources from './sources/sources';
+import { Article, AllSources } from '../../types/index';
 
 export class AppView {
+    news: News;
+    sources: Sources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: { articles: Article[] }) {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: AllSources) {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
