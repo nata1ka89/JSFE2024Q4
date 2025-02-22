da22f7583c7114cb538dad555e8d448eb69efd4d
nata1ka <nata1ka.yurevich@gmail.com>
2025-02-22 14:09:34 +0300

    refactor: add enum

--- news-api/src/types/index.ts
+++ news-api/src/types/index.ts
@@ -1,15 +1,103 @@
+enum RequestStatus {
+    Ok = 'ok',
+    Error = 'error',
+}
+
+enum Country {
+    AE = 'ae',
+    AR = 'ar',
+    AT = 'at',
+    AU = 'au',
+    BE = 'be',
+    BG = 'bg',
+    BR = 'br',
+    CA = 'ca',
+    CH = 'ch',
+    CN = 'cn',
+    CO = 'co',
+    CU = 'cu',
+    CZ = 'cz',
+    DE = 'de',
+    EG = 'eg',
+    FR = 'fr',
+    GB = 'gb',
+    GR = 'gr',
+    HK = 'hk',
+    HU = 'hu',
+    ID = 'id',
+    IE = 'ie',
+    IL = 'il',
+    IN = 'in',
+    IT = 'it',
+    JP = 'jp',
+    KR = 'kr',
+    LT = 'lt',
+    LV = 'lv',
+    MA = 'ma',
+    MX = 'mx',
+    MY = 'my',
+    NG = 'ng',
+    NL = 'nl',
+    NO = 'no',
+    NZ = 'nz',
+    PH = 'ph',
+    PL = 'pl',
+    PT = 'pt',
+    RO = 'ro',
+    RS = 'rs',
+    RU = 'ru',
+    SA = 'sa',
+    SE = 'se',
+    SG = 'sg',
+    SI = 'si',
+    SK = 'sk',
+    TH = 'th',
+    TR = 'tr',
+    TW = 'tw',
+    UA = 'ua',
+    US = 'us',
+    VE = 've',
+    ZA = 'za',
+}
+
+enum Language {
+    AR = 'ar',
+    DE = 'de',
+    EN = 'en',
+    ES = 'es',
+    FR = 'fr',
+    HE = 'he',
+    IT = 'it',
+    NL = 'nl',
+    NO = 'no',
+    PT = 'pt',
+    RU = 'ru',
+    SE = 'se',
+    UD = 'ud',
+    ZH = 'zh',
+}
+
+enum Category {
+    Business = 'business',
+    Entertainment = 'entertainment',
+    General = 'general',
+    Health = 'health',
+    Science = 'science',
+    Sports = 'sports',
+    Technology = 'technology',
+}
 export interface ISources {
     id: string;
     name: string;
     description: string;
     url: string;
-    category: string;
-    language: string;
-    country: string;
+    category: Category;
+    language: Language;
+    country: Country;
 }
 
 export interface SourcesResponse {
-    status: string;
+    status: RequestStatus;
     sources: ISources[];
 }
 
@@ -30,7 +118,7 @@ export interface Article {
 }
 
 export interface ArticleResponse {
-    status: string;
+    status: RequestStatus;
     totalResults: bigint;
     articles: Article[];
 }
