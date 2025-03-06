19ecb77cde2fdf3e1a1668797dfdba9d56d06b37
nata1ka <nata1ka.yurevich@gmail.com>
2025-03-06 16:51:20 +0300

    chore: add all config

---
+++ decision-making-tool/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+/*const CopyPlugin = require('copy-webpack-plugin');*/
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    /*assets: 'assets/[hash][ext]'*/
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[ts]j$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(wolff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'decision-making-too',
+    }),
+    new CleanWebpackPlugin(),
+    /*    new CopyPlugin({
+         patterns: [
+           { from: './public' }
+         ]
+       }) */
+  ],
+};
