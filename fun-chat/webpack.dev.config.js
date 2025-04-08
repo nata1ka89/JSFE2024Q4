362e356ef51420c42365f212f06fa3fddebaa630
nata1ka <nata1ka.yurevich@gmail.com>
2025-04-08 15:24:33 +0300

    chore: add all config

---
+++ fun-chat/webpack.dev.config.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.js');
+const path = require('path');
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+});
